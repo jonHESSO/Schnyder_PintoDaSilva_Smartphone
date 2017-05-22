a0de3d1496cb82e778ac9c37f9fb1d5b8fc35094
jonHESSO <jonathan.schnyder@students.hevs.ch>
2017-05-22 18:23:33 +0200

    Ajout des winrate morpion

---
+++ src/app_tictactoe/TicTacToeStatsTest.java
@@ -0,0 +1,23 @@
+/*
+ * Project : Schnyder_PintoDaSilva_Smartphone
+ * Author : Jonathan Schnyder
+ * Created : May 22, 2017
+ */
+
+package app_tictactoe;
+
+import java.util.ArrayList;
+
+public class TicTacToeStatsTest
+{
+
+	public static void main(String[] args)
+	{
+		TicTacToeStats stats = new TicTacToeStats() ;
+		stats.AIWins() ;
+		stats.P1WinsVAI();
+		stats.AIWins();
+		stats.showStatsVAI();
+	}
+
+}
