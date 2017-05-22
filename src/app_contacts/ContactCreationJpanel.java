f41076a54d1181cc8f8a088bfcad02772deddc23
samuelpintodas <samuel.pintodasilva@students.hevs.ch>
2017-05-22 11:31:22 +0200

    Contact:  affichage et modif panels
    modifications des dimensions des JLabels, des JTextfields et des JPanels

--- src/app_contacts/ContactCreationJpanel.java
+++ src/app_contacts/ContactCreationJpanel.java
@@ -1,79 +1,98 @@
 package app_contacts;
+
 //Imports
 import java.awt.event.*;
 
 //imports layouts
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 //imports J***
-import javax.swing.JButton;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.*;
 
-public abstract class ContactCreationJpanel extends JPanel {
-	
-	//Textfield --> attributs obligatoires pour un contact
-    private JTextField fieldFirstName;
-    private JTextField fieldLastName;
-    private JTextField fieldNumber;
-    
-    //Textfield --> attributs optionnels pour un contact
-    private JTextField fieldEmail;
-    private JTextField fieldImagePath;
-    
-    //Buttons OK et Cancel
-    private JButton okButton;
-    private JButton cancelButton;
-    
-    //Panel accueillant le panel avec les JTexfields et celui avec les JButtons
-    public void FormPanel() {
+public class ContactCreationJpanel extends JPanel {
 
-        setLayout(new BorderLayout());
-        add(contactFieldsPanel());
-        add(buttonsFieldsPanel(), BorderLayout.SOUTH);
-    }
-    
-    //Panel accueillant les buttons "Ok" et "Cancel"
-    public JPanel buttonsFieldsPanel() {
-    	
-        JPanel panel = new JPanel(new FlowLayout());
-        JButton CancelButton = new JButton("Cancel");
-        cancelButton.addActionListener(new Cancel_Click());
-        //Ajout des buttons
-        panel.add((okButton = new JButton("Ok")));
-        panel.add(cancelButton);
+	// Panel accueillant le panel avec les JTexfields et celui avec les JButtons
+	public ContactCreationJpanel() {
 
-        return panel;
-    }
-    
-	class Cancel_Click implements ActionListener{
-		public void actionPerformed(ActionEvent e){
-			int ret = JOptionPane.showConfirmDialog(null, "Voulez-vous annuler la création du contact ?", "Confirmation d'annulation"
-													, JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+		this.setPreferredSize(new Dimension(500, 800));
+		setLayout(new BorderLayout());
+		add(fieldsLabelsPanel(), BorderLayout.WEST);
+		add(contactFieldsPanel(), BorderLayout.EAST);
+		add(buttonsPanel(), BorderLayout.SOUTH);
+	}
+
+	//Panel accueillant les labels des textFields
+	public JPanel fieldsLabelsPanel() {
+		JPanel panel = new JPanel(new FlowLayout());
+		panel.setPreferredSize(new Dimension(50, 500));
+		panel.add(new JLabel ("Prénom")).setPreferredSize(new Dimension(50, 20));
+		panel.add(new JLabel ("Nom")).setPreferredSize(new Dimension(50, 20));
+		panel.add(new JLabel ("Numéro")).setPreferredSize(new Dimension(50, 20));
+		panel.add(new JLabel ("E-Mail")).setPreferredSize(new Dimension(50, 20));
+		panel.add(new JLabel ("Image")).setPreferredSize(new Dimension(50, 20));
 		
-		//	if (ret == JOptionPane.YES_OPTION)
-				//Ferme uniquement la fenêtre:
-				//a faire plus tard, revient sur la fenetre precedente (liste de contacts)
+		
+
+		return panel;
+	}
+
+	// Panel accueillant les buttons "Ok" et "Cancel"
+	public JPanel buttonsPanel() {
+
+		JPanel panel = new JPanel(new FlowLayout());
+		JButton okButton = new JButton("OK");
+		JButton cancelButton = new JButton("Cancel");
+		cancelButton.addActionListener(new Cancel_Click());
+		// Ajout des buttons
+		panel.add(okButton);
+		panel.add(cancelButton);
+
+		return panel;
+	}
+
+	class Cancel_Click implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			int ret = JOptionPane.showConfirmDialog(null, "Voulez-vous annuler la création du contact ?",
+					"Confirmation d'annulation", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+
+			// if (ret == JOptionPane.YES_OPTION)
+			// Ferme uniquement la fenêtre:
+			// a faire plus tard, revient sur la fenetre precedente (liste de
+			// contacts)
 		}
 	}
-    
 
-	//Panel contenant les textfield
-	
-    public JPanel contactFieldsPanel(){
-    	JPanel panel = new JPanel(new FlowLayout());
-    	//Ajout des textfields
-    		//attributs obligatoires pour un contact
-    		panel.add(fieldFirstName);
-    		panel.add(fieldLastName);
-    		panel.add(fieldNumber);
-    		//attributs optionnels pour un contact
-    		panel.add(fieldEmail);
-    		panel.add(fieldImagePath);
-    	return panel;
-    }
+	// Panel contenant les textfield
+
+	public JPanel contactFieldsPanel() {
+		JPanel panel = new JPanel(new FlowLayout());
+		panel.setPreferredSize(new Dimension(200, 500));
+		// Ajout des textfields
+		// Textfield --> attributs obligatoires pour un contact
+		JTextField fieldFirstName = new JTextField(25);
+		fieldFirstName.setPreferredSize(new Dimension(50, 20));
+		JTextField fieldLastName = new JTextField(25);
+		fieldFirstName.setPreferredSize(new Dimension(50, 20));
+		JTextField fieldNumber = new JTextField(25);
+		fieldFirstName.setPreferredSize(new Dimension(50, 20));
+
+		// Textfield --> attributs optionnels pour un contact
+		JTextField fieldEmail = new JTextField(25);
+		fieldFirstName.setPreferredSize(new Dimension(50, 20));
+		JTextField fieldImagePath = new JTextField(25);
+		fieldFirstName.setPreferredSize(new Dimension(50, 20));
+		// attributs obligatoires pour un contact
+		panel.add(fieldFirstName);
+		panel.add(fieldLastName);
+		panel.add(fieldNumber);
+		// attributs optionnels pour un contact
+		panel.add(fieldEmail);
+		panel.add(fieldImagePath);
+		return panel;
+	}
 }
