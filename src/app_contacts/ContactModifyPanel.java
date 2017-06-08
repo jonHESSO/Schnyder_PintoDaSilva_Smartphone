cd1e0c7fb0eb664043e80471857e6d064f755f12
jonHESSO <jonathan.schnyder@students.hevs.ch>
2017-06-08 15:29:43 +0200

    small update

--- src/app_contacts/ContactModifyPanel.java
+++ src/app_contacts/ContactModifyPanel.java
@@ -15,12 +15,12 @@ import ressources.Ressources;
 import ressources.Serializer;
 
 //Permet de modifier un contact déjà existant
-public class ContactModifyJPanel extends ContactEditPanel {
+public class ContactModifyPanel extends ContactEditPanel {
 
 	private Contact currentContact;
 
 	// Constructeur du panel
-	public ContactModifyJPanel(Contact contact) {
+	public ContactModifyPanel(Contact contact) {
 		super();
 
 		this.currentContact = contact;
@@ -30,25 +30,19 @@ public class ContactModifyJPanel extends ContactEditPanel {
 		fieldLastName.setText(contact.getLastName());
 		fieldNumber.setText(contact.getNumber());
 		fieldEmail.setText(contact.getEmail());
-		fieldImagePath.setText(contact.getImagePath());
+		imageButton.setIcon(contact.getPicture());
 	}
 
 	// Listener du cancelButton
 	@Override
 	public void okAction() {
 		//Sauvegarde des modif.
-		this.currentContact.modify();
+		String lastName = fieldLastName.getText() ;
+		String firstName = fieldFirstName.getText() ;
+		String number = fieldNumber.getText() ;
+		String email = fieldEmail.getText() ;
+		Ressources.CONTACTLIST.modifyContact(currentContact, lastName, firstName, number, email, null);
 		Serializer.serializableObject(Ressources.CONTACTLIST, Ressources.CONTACT_DIRECTORY);
 	}
 
-	// Listener du button Cancel
-	@Override
-	public void cancelAction() {
-		int reponse = JOptionPane.showConfirmDialog(null, "Realy want to quit bro?", "Confirmation",
-				JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
-		if (reponse == JOptionPane.YES_OPTION) {
-			ContactModifyJPanel.this.setVisible(false);
-		}
-		
-	}
 }
