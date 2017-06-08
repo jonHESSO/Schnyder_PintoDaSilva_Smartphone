6a598dabb3258765b516afacf253ecb47a1ce590
jonHESSO <jonathan.schnyder@students.hevs.ch>
2017-06-08 15:37:14 +0200

    step 3

--- src/app_contacts/ContactModifyPanel.java
+++ src/app_contacts/ContactModifyPanel.java
@@ -14,13 +14,9 @@ import javax.swing.JTextField;
 import ressources.Ressources;
 import ressources.Serializer;
 
-<<<<<<< HEAD:src/app_contacts/ContactModifyPanel.java
 //Permet de modifier un contact déjà existant
 public class ContactModifyPanel extends ContactEditPanel {
-=======
-//Permet de modifier un contact dÃ©jÃ  existant
-public class ContactModifyJPanel extends ContactEditPanel {
->>>>>>> 4f93b0c01cc1c2aa7c81efebd2bfb017b3c615e3:src/app_contacts/ContactModifyJPanel.java
+
 
 	private Contact currentContact;
 
@@ -49,4 +45,10 @@ public class ContactModifyJPanel extends ContactEditPanel {
 		Ressources.CONTACTLIST.modifyContact(currentContact, lastName, firstName, number, email, null);
 		Serializer.serializableObject(Ressources.CONTACTLIST, Ressources.CONTACT_DIRECTORY);
 	}
+
+	@Override
+	public void cancelAction()
+	{
+	}
+}
 
